a900e65e410321ffce80cc521a8cdf51e432d2a1
Thomas Humphrey <thumphrey9123@gmail.com>
2020-01-03 16:32:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,8 @@
 def snake_it_up(string)
-  if string[0] == "s"
-   string = 10.times { "s" + string}
-  else
+  # if string[0] == "s"
+  # string = 10.times { "s" + string}
+  # else
+  # string
+  # end
   string
-  end
 end
